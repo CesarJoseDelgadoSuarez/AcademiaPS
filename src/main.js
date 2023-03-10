d276207462ad42786128556b93b1d7bd767b05d9
César José Delgado Suárez <cjdsgithub@gmail.com>
2023-03-10 16:50:25 +0000

    added bootstrap and deleted components

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import './assets/main.css'
+
+// Importamos Bootstrap 
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createApp(App).mount('#app')
